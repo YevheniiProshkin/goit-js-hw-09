366ed566f6800fc58bc9018e05aa15f79a3dac2c
YevheniiProshkin <evgeniyprofi@ukr.net>
2022-08-24 19:23:36 +0300

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
 
+startBtnEl.addEventListener('click', onStartBtnClick);
+stopBtnEl.addEventListener('click', onStopBtnClick);
+
+let colorIntervalId = null;
+
+function onStartBtnClick(e) {
+  colorIntervalId = setInterval(changeBgColor, 1000);
+  e.target.disabled = true;
+  stopBtnEl.disabled = false;
+}
+
+function onStopBtnClick(e) {
+  clearInterval(colorIntervalId);
+  e.target.disabled = true;
+  startBtnEl.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
